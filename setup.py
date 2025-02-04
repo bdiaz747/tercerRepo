3a3d8bc52a0fddb343ec28a1dfd171a448b655c9
Brahian Diaz G <brahian.diazgarcia@gmail.com>
2025-02-03 21:23:18 -0500

    Correo editado

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     packages=find_packages(),                       # Paquetes a incluir
     description="Un paquete pip simple de saludo",  # Breve descripción
     author="Brahian Díaz G.",                       # Tu nombre
-    author_email="brahian.diazgarcia@gmail.",       # Tu correo electrónico
+    author_email="brahian.diazgarcia@gmail.com",    # Tu correo electrónico
     url="https://github.com/bdiaz747/tercerRepo",   # URL del proyecto
 )
