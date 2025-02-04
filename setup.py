011ea1f782b580df9cc3de457ca2790ea84ada19
Brahian Diaz G <brahian.diazgarcia@gmail.com>
2025-02-03 20:15:41 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Brahian Díaz G.",                       # Tu nombre
+    author_email="brahian.diazgarcia@gmail.",       # Tu correo electrónico
+    url="https://github.com/bdiaz747/tercerRepo",   # URL del proyecto
+)
